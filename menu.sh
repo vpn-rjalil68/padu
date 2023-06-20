526c7d8357fb11ffd44cf6b8f644fcca67b2cc0b
vpn-rjalil68 <136805734+vpn-rjalil68@users.noreply.github.com>
2023-06-20 22:28:16 +0800

    Add files via upload

--- menu.sh
+++ menu.sh
@@ -10,13 +10,9 @@ PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 NC='\033[0;37m'
 clear
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 IZIN=$( curl https://raw.githubusercontent.com/vpn-rjalil68/ip/main/access | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
-echo -e ""
 # DNS Patch
 tipeos2=$(uname -m)
 # OS Uptime
